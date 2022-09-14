ac998b91f4aefbd59d6a2039b6d8a87b9378213a
sys72740hro <Hcbdag@635>
2022-09-14 09:43:13 +0200

    Login page en registration

--- src/Controller/Admin/ChamberCrudController.php
+++ src/Controller/Admin/ChamberCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Chamber;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ChamberCrudController extends AbstractCrudController
 {
@@ -12,14 +15,13 @@ class ChamberCrudController extends AbstractCrudController
         return Chamber::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+        public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('name'),
             TextEditorField::new('description'),
+            MoneyField::new('price')->setCurrency('EUR'),
         ];
     }
-    */
+
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class DefaultController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        // Avoid calling getUser() in the constructor: auth may not
+        // be complete yet. Instead, store the entire Security object.
+        $this->security = $security;
+    }
+
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'MBO RIJNLAND Zoetermeer',
-            'name' => 'Henry Robben',
-        ]);
+        if ($this->security->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->render('default/index.html.twig', [
+                'controller_name' => 'MBO RIJNLAND Zoetermeer',
+                'name' => 'Henry Robben',
+            ]);
+        }
+        return $this->redirectToRoute('app_login');
     }
 }
